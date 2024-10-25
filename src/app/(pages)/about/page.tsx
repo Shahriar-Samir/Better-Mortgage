f81487abd57e86376273c1bb41e2fb0b13a4b90c
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-25 20:20:34 +0600

    add: timeline section of about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -2,6 +2,7 @@ import Changing from '@/app/(components)/Aboutpage/Changing';
 import Companies from '@/app/(components)/Aboutpage/Companies';
 import Mission from '@/app/(components)/Aboutpage/Mission';
 import Story from '@/app/(components)/Aboutpage/Story';
+import Timeline from '@/app/(components)/Aboutpage/Timeline';
 import Navbar from '@/app/(components)/navbar';
 import React from 'react';
 
@@ -13,6 +14,7 @@ const page = () => {
             <Story/>
             <Changing/>
             <Companies/>
+            <Timeline/>
         </main>
     );
 };
