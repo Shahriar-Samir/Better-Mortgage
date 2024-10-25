1c7c8d744fe3acaf87bd1c491c7557b92219263e
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-25 23:04:17 +0600

    add: changing section of about page designed

--- src/app/(components)/Aboutpage/Changing.tsx
+++ src/app/(components)/Aboutpage/Changing.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Changing = () => {
     return (
-        <section>
-            <h1>How we’re changing things</h1>
-            <h2><p>
+        <section className='bg-[#017848] p-24 text-white mt-36'>
+            <h1 className='text-5xl font-myFont5'>How we’re changing things</h1>
+            <h2 className='mt-8 font-myFont3'><p>
   Homeownership is a huge part of our economy. Housing overall is a $33 trillion business, and mortgages account for $15 trillion. Yet home finance operates in the same way it has for decades — through opaque systems and expensive intermediaries whose interests are misaligned with consumers’.
-</p>
+</p><br></br>
 <p>
   That’s why Better.com is redefining the homeownership process from the ground up. We’re using technology to make it faster and more efficient, and humans to help make it friendly and enjoyable.
 </p>
