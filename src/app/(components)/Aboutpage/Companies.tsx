2cffd390d53cab296b87c1e2d82b19c65fcf546c
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-25 23:10:08 +0600

    add: companies section

--- src/app/(components)/Aboutpage/Companies.tsx
+++ src/app/(components)/Aboutpage/Companies.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Companies = () => {
     return (
-        <section>
-            <h1>Backed by</h1>
-            <div className='w-full flex gap-5'>
+        <section className='mt-28'>
+            <h1 className='text-center text-3xl font-myFont5'>Backed by</h1>
+            <div className='w-full flex gap-2 justify-center mt-20'>
                 <img src='/companies/company1.svg'/>
                 <img src='/companies/company2.svg'/>
                 <img src='/companies/company3.svg'/>
                 <img src='/companies/company4.svg'/>
                 <img src='/companies/company5.svg'/>
+                <img src='/companies/company7.svg'/>
                 <img src='/companies/company6.svg'/>
             </div>
         </section>
