42bd01cd2cb9fd580ff43e2a0eba4f47f5290f71
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-25 23:46:22 +0600

    add: timeline design of about page

--- src/app/(components)/Aboutpage/Timeline.tsx
+++ src/app/(components)/Aboutpage/Timeline.tsx
@@ -2,130 +2,149 @@ import React from 'react';
 
 const Timeline = () => {
     return (
-        <div>
-            <h1>Company Timeline</h1>
-            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        <section className='mt-32'>
+              <h1 className='text-center text-3xl font-myFont5'>Company timeline</h1>
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical mt-16">
   <li>
     <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
+    <button className='mt-4 py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2014</button>
     </div>
-    <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">1984</time>
-      <div className="text-lg font-black">First Macintosh computer</div>
-      The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
-      personal computer. It played a pivotal role in establishing desktop publishing as a general
-      office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
-      in a beige case with integrated carrying handle; it came with a keyboard and single-button
-      mouse.
+    <div className="timeline-start mb-10 mt-16">
+      <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+      After Vishal Garg’s first attempt to purchase his own dream home, he quickly realized that the homebuying process is unnecessarily broken. This inspired him to found a technology-first company led by engineers and data experts with the mission of digitizing and automating home finance to make it cheaper, easier, and faster for all.
+      </div>
     </div>
     <hr />
   </li>
   <li>
     <hr />
     <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2015</button>
     </div>
-    <div className="timeline-end mb-10">
-      <time className="font-mono italic">1998</time>
-      <div className="text-lg font-black">iMac</div>
-      iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has
-      been the primary part of Apple's consumer desktop offerings since its debut in August 1998,
-      and has evolved through seven distinct forms
+    <div className="timeline-end mb-10 mt-12">
+      <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+      Better Mortgage funds its first mortgage loan entirely online (without a single phone call!).
+      </div>
     </div>
     <hr />
   </li>
   <li>
     <hr />
     <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2016</button>
     </div>
-    <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">2001</time>
-      <div className="text-lg font-black">iPod</div>
-      The iPod is a discontinued series of portable media players and multi-purpose mobile devices
-      designed and marketed by Apple Inc. The first version was released on October 23, 2001, about
-      8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450
-      million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At
-      over 20 years, the iPod brand is the oldest to be discontinued by Apple
+    <div className="timeline-start mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage becomes a Fannie Mae approved seller + servicer and establishes relationships with top mortgage investors.
+      </div>
     </div>
     <hr />
   </li>
   <li>
     <hr />
     <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2017</button>
     </div>
-    <div className="timeline-end mb-10">
-      <time className="font-mono italic">2007</time>
-      <div className="text-lg font-black">iPhone</div>
-      iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
-      operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on
-      January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As
-      of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone
-      accounts for 15.6% of global smartphone market share
+    <div className="timeline-end mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better expands into the real estate market with Better Real Estate.
+      </div>
     </div>
     <hr />
   </li>
   <li>
     <hr />
     <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2018</button>
     </div>
-    <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">2015</time>
-      <div className="text-lg font-black">Apple Watch</div>
-      The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness
-      tracking, health-oriented capabilities, and wireless telecommunication, and integrates with
-      iOS and other Apple products and services
+    <div className="timeline-start mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage partners with Ally Bank to build Ally powered by Better.
+      </div>
     </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2019</button>
+    </div>
+    <div className="timeline-end mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage launches its pilot partnership with American Express to deliver a seamless homebuying experience to AMEX customers.
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2021</button>
+    </div>
+    <div className="timeline-start mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better acquires Trussle — The UK’s most innovative online mortgage broker.
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2022</button>
+    </div>
+    <div className="timeline-end mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage becomes the first fintech to fund $100B home loans entirely online.
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>2023</button>
+    </div>
+    <div className="timeline-start mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage launches One Day Mortgage¹: The first offering to customers to go from application to full mortgage Commitment Letter within 24 hours vs. typical industry process of 30+ days.
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-end ms-16 mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage launches the fully digital 3-day HELOC².
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start me-16 mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md'>
+    Better Mortgage launches One Day Verified Approval Letter.
+      </div>
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+    <button className='py-2 px-8 bg-[#017848] text-2xl font-myFont5 rounded-full text-white'>Today</button>
+    </div>
+    <div className="timeline-end mb-10 mt-12">
+    <div className='mt-6 p-10 bg-[#DFE0DC] w-[370px] text-start font-myFont3 text-gray-500 rounded-md flex flex-col'>
+    You become part of the story by joining tens of thousands of happy Better Mortgage borrowers.
+    <button className='w-fit mt-4 p-4 py-3 bg-[#017848] text-lg font-myFont5 rounded-md text-white'>Get started</button>
+      </div>
+    </div>
+    <hr />
   </li>
 </ul> 
-        </div>
+        </section>
     );
 };
 
